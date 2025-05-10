dfe2b97a69a3c7a9afd4c4160731c318a89ecb33
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2025-05-10 23:10:05 +0900

    Wrap the entire app with SafeAreaView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaView>
   );
 }
